8f066a15b1690b4a95667923b698b4194ae115e8
bogdan <bogdanplytus@gmail.com>
2026-02-05 20:47:48 +0200

    фир

--- buba/urls.py
+++ buba/urls.py
@@ -1,51 +1,35 @@
-"""
-URL configuration for biba project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('portfolio/add/', views.add_project, name='add_project')
-    path('', HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
+
+    path('login/', views.user_login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.user_logout, name='logout'),
+
+    path('profile/', views.profile_view, name='profile'),
+
+    path('portfolio/', views.portfolio_view, name='portfolio'),
+    path('portfolio/add/', views.add_project, name='add_project'),
+
     path('gallery/', views.gallery_view, name='gallery'),
     path('gallery/add/', views.add_gallery_photo, name='add_gallery_photo'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('profile/', ProfileView.as_view(), name='profile'),
 
-    path('forum/', TopicListView.as_view(), name='forum'),
-    path('forum/<int:pk>/', TopicDetailView.as_view(), name='topic_detail'),
-    path('forum/<int:pk>/post/', PostCreateView.as_view(), name='post_create'),
+    path('forum/', views.TopicListView.as_view(), name='forum'),
+    path('forum/<int:pk>/', views.TopicDetailView.as_view(), name='topic_detail'),
+    path('forum/<int:pk>/post/', views.PostCreateView.as_view(), name='post_create'),
+    path('grades/', views.GradeListView.as_view(), name='grades'),
+    path('grades/add/', views.GradeCreateView.as_view(), name='grade_add'),
 
-    path('grades/', GradeListView.as_view(), name='grades'),
-    path('grades/add/', GradeCreateView.as_view(), name='grade_add'),
+    path('events/', views.EventListView.as_view(), name='events'),
+    path('events/add/', views.EventCreateView.as_view(), name='event_add'),
+    path('events/<int:pk>/edit/', views.EventUpdateView.as_view(), name='event_edit'),
+    path('events/<int:pk>/delete/', views.EventDeleteView.as_view(), name='event_delete'),
 
-    path('events/', EventListView.as_view(), name='events'),
-    path('events/add/', EventCreateView.as_view(), name='event_add'),
-    path('events/<int:pk>/edit/', EventUpdateView.as_view(), name='event_edit'),
-    path('events/<int:pk>/delete/', EventDeleteView.as_view(), name='event_delete'),
+    path('poll/<int:pk>/', views.PollView.as_view(), name='poll'),
+    path('vote/<int:pk>/', views.VoteView.as_view(), name='vote'),
 
-    path('poll/<int:pk>/', PollView.as_view(), name='poll'),
-
-    path('vote/<int:pk>/', VoteView.as_view(), name='vote'),
-
-    path('announcements/', AnnouncementListView.as_view(), name='announcements'),
-    path('materials/', MaterialListView.as_view(), name='materials'),
-    path('portfolio/', PortfolioListView.as_view(), name='portfolio'),
-
-    path('gallery/', GalleryListView.as_view(), name='gallery'),
+    path('announcements/', views.AnnouncementListView.as_view(), name='announcements'),
+    path('materials/', views.MaterialListView.as_view(), name='materials'),
 ]
--- buba/views.py
+++ buba/views.py
@@ -1,25 +1,117 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from .models import GalleryImage, Project
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, "index.html")
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, f'Вітаємо, {user.username}!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Неправильний логін або пароль.')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Реєстрація успішна!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Помилка реєстрації.')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration.html', {'form': form})
 
 
 @login_required
-def add_gallery_photo(request):
-    if request.method == 'POST':
-        photo = request.FILES.get('photo')
-        description = request.POST.get('description', '')
+def user_logout(request):
+    logout(request)
+    messages.info(request, 'Ви вийшли з акаунту.')
+    return redirect('home')
 
-        if photo:
-            image = GalleryImage(
-                user=request.user,
-                photo=photo,
-                description=description
-            )
-            image.save()
-            messages.success(request, 'Фото успішно додано в галерею!')
-        else:
-            messages.error(request, 'Оберіть фото для завантаження.')
+@login_required
+def profile_view(request):
+    return render(request, "users/profile.html")
 
-    return redirect('gallery')
+class TopicListView(View):
+    def get(self, request):
+        return render(request, "forum/topic_list.html")
+
+
+class TopicDetailView(View):
+    def get(self, request, pk):
+        return render(request, "forum/topic_detail.html", {"topic_id": pk})
+
+
+class PostCreateView(View):
+    def get(self, request, pk):
+        return render(request, "forum/post_create.html", {"topic_id": pk})
+
+class GradeListView(View):
+    def get(self, request):
+        return render(request, "diary/grade_list.html")
+
+
+class GradeCreateView(View):
+    def get(self, request):
+        return render(request, "diary/grade_add.html")
+
+class EventListView(View):
+    def get(self, request):
+        return render(request, "events.html")
+
+
+class EventCreateView(View):
+    def get(self, request):
+        return render(request, "events/event_add.html")
+
+
+class EventUpdateView(View):
+    def get(self, request, pk):
+        return render(request, "events/event_edit.html", {"event_id": pk})
+
+
+class EventDeleteView(View):
+    def get(self, request, pk):
+        return render(request, "events/event_delete.html", {"event_id": pk})
+
+class PollView(View):
+    def get(self, request, pk):
+        return render(request, "polls/poll.html", {"poll_id": pk})
+
+
+class VoteView(View):
+    def get(self, request, pk):
+        return render(request, "voting/vote.html", {"vote_id": pk})
+
+class AnnouncementListView(View):
+    def get(self, request):
+        return render(request, "announcements.html")
+
+
+class MaterialListView(View):
+    def get(self, request):
+        return render(request, "materials/list.html")
 
 @login_required
 def portfolio_view(request):
@@ -30,7 +122,7 @@ def portfolio_view(request):
 @login_required
 def add_project(request):
     if request.method == 'POST':
-        project = Project(
+        Project.objects.create(
             user=request.user,
             title=request.POST.get('title'),
             description=request.POST.get('description'),
@@ -39,107 +131,29 @@ def add_project(request):
             github_url=request.POST.get('github_url', ''),
             image=request.FILES.get('image')
         )
-        project.save()
-
+        messages.success(request, 'Проєкт додано!')
     return redirect('portfolio')
-
-# Головна сторінка
-class HomeView(View):
-    def get(self, request):
-        return render(request, "index.html")
-class RegisterView(View):
-    def get(self, request):
-        return render(request, "users/register.html")
-
-
-class UserLoginView(View):
-    def get(self, request):
-        return render(request, "users/login.html")
-
-
-# Вихід із системи
-class UserLogoutView(View):
-    def get(self, request):
-        return render(request, "users/logout.html")
-
-
-# Профіль користувача
-class ProfileView(View):
-    def get(self, request):
-        return render(request, "users/profile.html")
-
-class TopicListView(View):
-    def get(self, request):
-        return render(request, "forum/topic_list.html")
-
-
-# Перегляд однієї теми
-class TopicDetailView(View):
-    def get(self, request, pk):
-        return render(request, "forum/topic_detail.html", {"topic_id": pk})
-
-
-# Додавання повідомлення
-class PostCreateView(View):
-    def get(self, request, pk):
-        return render(request, "forum/post_create.html", {"topic_id": pk})
-
-# Список оцінок
-class GradeListView(View):
-    def get(self, request):
-        return render(request, "diary/grade_list.html")
-
-
-# Додавання оцінки
-class GradeCreateView(View):
-    def get(self, request):
-        return render(request, "diary/grade_add.html")
-
-class EventListView(View):
-    def get(self, request):
-        return render(request, "events.html")
-
-
-# Додавання події
-class EventCreateView(View):
-    def get(self, request):
-        return render(request, "events/event_add.html")
-
-class EventUpdateView(View):
-    def get(self, request, pk):
-        return render(request, "events/event_edit.html", {"event_id": pk})
-
-
-# Видалення події
-class EventDeleteView(View):
-    def get(self, request, pk):
-        return render(request, "events/event_delete.html", {"event_id": pk})
-
-# Перегляд опитування
-class PollView(View):
-    def get(self, request, pk):
-        return render(request, "polls/poll.html", {"poll_id": pk})
-class VoteView(View):
-    def get(self, request, pk):
-        return render(request, "voting/vote.html", {"vote_id": pk})
-class AnnouncementListView(View):
-    def get(self, request):
-        return render(request, "announcements.html")
-
-
-class MaterialListView(View):
-    def get(self, request):
-        return render(request, "materials/list.html")
-
-class PortfolioListView(View):
-    def get(self, request):
-        return render(request, "portfolio.html")
-
-
-class GalleryListView(View):
-    def get(self, request):
-        return render(request, "gallery.html")
 
 def gallery_view(request):
     images = GalleryImage.objects.filter(is_public=True).select_related('user')
     return render(request, 'gallery.html', {'all_gallery_images': images})
+
+
+@login_required
+def add_gallery_photo(request):
+    if request.method == 'POST':
+        photo = request.FILES.get('photo')
+        description = request.POST.get('description', '')
+
+        if photo:
+            GalleryImage.objects.create(
+                user=request.user,
+                photo=photo,
+                description=description
+            )
+            messages.success(request, 'Фото додано в галерею!')
+        else:
+            messages.error(request, 'Оберіть фото.')
+
+    return redirect('gallery')
+
